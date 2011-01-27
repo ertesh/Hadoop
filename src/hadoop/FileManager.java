51973520d62da9ae3f668997e86fcbf0a9050963
Maciej Andrejczuk <maciej.andrejczuk@gmail.com>
2011-01-27 13:17:54 +0100

    Refactoring. Whole API moved to separate GWT project.

--- src/hadoop/FileManager.java
+++ src/hadoop/FileManager.java
@@ -8,19 +8,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobConf;
-
-import api.Grade;
-import api.Movie;
-import api.User;
 
 public class FileManager {
 	public static final String inPath = "input";
@@ -50,62 +44,51 @@ public class FileManager {
 		// The directory is now empty so delete it
 		return dir.delete();
 	}
-	
-	public static List<Path> getFiles (Path path, Configuration conf) {
-		List<Path> list = new ArrayList<Path> ();
+
+	public static List<Path> getFiles(Path path, Configuration conf) {
+		List<Path> list = new ArrayList<Path>();
 		try {
 			FileStatus[] fsa = FileSystem.get(conf).listStatus(path);
 			for (FileStatus fs : fsa) {
 				if (!fs.isDir())
 					list.add(fs.getPath());
 			}
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return list;
 	}
-	
+
 	public static Path[] checkFolder(Path[] cacheFiles, JobConf conf, boolean b) {
 		if (cacheFiles != null)
 			return cacheFiles;
 
 		String path = centersPath + "0";
-		if (b) path = clustersPath + "1";
+		if (b)
+			path = clustersPath + "1";
 		return getFiles(new Path(path), conf).toArray(new Path[1]);
 
 	}
 
-	public static void saveGrades(Map<User, List<Grade>> grades) {
+	public static void saveGrades(List<String> grades) {
 		String filename = inPath + "/" + "file01";
 		FileWriter fWriter;
 		try {
 			fWriter = new FileWriter(filename);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		BufferedWriter bWriter = new BufferedWriter(fWriter);
-		for (Map.Entry<User, List<Grade>> entry: grades.entrySet()) {
-			for (Grade grade: entry.getValue()) {
-				try {	
-					bWriter.write(grade.code());
-					bWriter.write("\n");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			BufferedWriter bWriter = new BufferedWriter(fWriter);
+			for (String grade : grades) {
+				bWriter.write(grade + "\n");
 			}
-		}
-		try {
 			bWriter.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public static Map<User, List<Grade>> readRecommendations() {
-		Map<User, List<Grade>> ret = new HashMap<User, List<Grade>>();
+	public static List<String> readRecommendations() {
+		
+		List<String> ret = new ArrayList<String>();
+
 		String filename = outPath + "/" + "part-00000";
 		FileReader fReader;
 		try {
@@ -118,22 +101,10 @@ public class FileManager {
 		String line;
 		try {
 			while ((line = bReader.readLine()) != null) {
-				String[] tab = line.split("\\s+");
-				int userId = Integer.parseInt(tab[0]);
-				int movieId = Integer.parseInt(tab[1]);
-				int grade = Integer.parseInt(tab[2]);
-				User user = User.getUser(userId);
-				Movie movie = Movie.getMovie(movieId);
-				Grade recommendation = new Grade(user, movie, grade);
-				if (!ret.containsKey(user)) {
-					List<Grade> list = new ArrayList<Grade>();
-					list.add(recommendation);
-					ret.put(user, list);
-				} else {
-					ret.get(user).add(recommendation);
-				}
+				ret.add(line);
 			}
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return ret;
 		}
